ae68b5ece46bbfdb9c8eced42c5fc6bccb6dc779
mrinmay7875 <mrinmay7875@gmail.com>
2021-12-14 20:23:21 +0530

    Modified Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,38 @@
-import React from 'react';
-
+import React from "react";
+import Link from "next/link";
 function Footer() {
-    return (
-        <div>
-            <footer
-                className="bg-green-700
+  return (
+    <div>
+     <footer
+        className="bg-gray-900 
              text-2xl text-black text-center
              fixed
              inset-x-0
              bottom-0
-             p-4 "
-            >
-                This is sticky fixed Footer.
-            </footer>
+             "
+      >
+        <div className=" text-center text-white pb-3 ">
+          <Link passHref={true} href="/">
+            <a className="hover:opacity-90 opacity-50 inline  font-semibold text-base ">
+              Home
+            </a>
+          </Link>
+
+          <div className="relative inline">
+            <div className="absolute text-5xl px-1 bottom-0 left-0 opacity-60">
+              <p>.</p>
+            </div>
+          </div>
+          <Link passHref={true} href="/feed">
+            <a className="hover:opacity-90 opacity-50 inline px-5 font-semibold text-base ">
+              Feed
+            </a>
+          </Link>
         </div>
-    );
+
+      </footer>
+    </div>
+  );
 }
 
 export default Footer;
